17a992c25c0894a7525671401ea564f516512ade
MichaelDik <mwdik@comcast.net>
2025-11-23 07:30:41 -0500

    Initial Comitt

---
+++ Examples/agent.py
@@ -0,0 +1,19 @@
+# pip install -qU "langchain[anthropic]" to call the model
+
+from langchain.agents import create_agent
+""" First agent from langcahin"""
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="claude-sonnet-4-5-20250929",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+# Run the agent
+agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
