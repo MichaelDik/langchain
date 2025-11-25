31337fd81e4a001d90783a26cd0b1a5685cf8954
MichaelDik <mwdik@comcast.net>
2025-11-25 07:20:22 -0500

    Lesson 1 from quickstart

--- Examples/QuickStartPythonEssentialsCourse/Lesson1.py
+++ Examples/QuickStartPythonEssentialsCourse/Lesson1.py
@@ -3,6 +3,11 @@ import operator
 from typing import Annotated, List, Literal, TypedDict
 from langgraph.graph import END, START, StateGraph
 from langgraph.types import Command, interrupt
+""" 
+
+Lesson 1 from courses, building state, nodes, edges. Very Simple
+
+"""
 
 class State(TypedDict):
     nlist: List[str]
@@ -12,25 +17,18 @@ def node_a(state: State) -> State:
     note = "\nHello World from Node B"
     return(State(nlist = [note]))
 
-def node_b(state: State) -> State: 
-    print(f"Node b is receiving {state['nlist']}")
-    note ="Sup"
-    return(State(nlist = [note]))
-
 builder = StateGraph(State)
 builder.add_node("a", node_a)
-#builder.add_node()
 builder.add_edge(START, "a")
 builder.add_edge("a", END)
 graph = builder.compile()
 
 
-display(Image(graph.get_graph().draw_mermaid_png()))
-#print(graph.get_graph().draw_mermaid())
+#display(Image(graph.get_graph().draw_mermaid_png()))
+print(graph.get_graph().draw_mermaid())
 
 initial_state = State(
-    nlist = ["Hello Node b, how are you?", "I am good how are you?"]
+    nlist = ["Hello Node a, how are you?"]
 )
-
-graph.invoke(initial_state)
-
+#graph.invoke(initial_state)
+print(type(builder))
